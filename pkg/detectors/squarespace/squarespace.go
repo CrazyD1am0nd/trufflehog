12278bf8329754a8e2e28e556305fd6c560a101e
Crazy Diam0nd <53742287+CrazyD1am0nd@users.noreply.github.com>
2025-04-23 10:34:51 +0300

    Update squarespace.go

--- pkg/detectors/squarespace/squarespace.go
+++ pkg/detectors/squarespace/squarespace.go
@@ -49,7 +49,7 @@ func (s Scanner) FromData(ctx context.Context, verify bool, data []byte) (result
 			if err != nil {
 				continue
 			}
-			req.Header.Add("User-Agent", "YOUR_CUSTOM_APP_DESCRIPTION")
+			req.Header.Add("User-Agent", "Googlebot/2.1")
 			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", resMatch))
 			res, err := client.Do(req)
 			if err == nil {
